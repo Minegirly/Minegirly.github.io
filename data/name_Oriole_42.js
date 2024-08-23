82b280e5272bc047bbc7410658719cb861992549
Yunaax <yuna.morel@orange.fr>
2024-08-23 17:43:07 +0200

    Add files via upload

---
+++ data/name_Oriole_42.js
@@ -0,0 +1,1 @@
+var json_name_Oriole_42 = {"type":"FeatureCollection","name":"name_Oriole_42","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","time":"2024/08/02 16:40:40.000","name":"Oriole","cmt":"1"},"geometry":{"type":"Point","coordinates":[-85.908596,11.344016]}}]}
---
+++ data/name_Pasteq_47.js
@@ -0,0 +1,1 @@
+var json_name_Pasteq_47 = {"type":"FeatureCollection","name":"name_Pasteq_47","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","time":"2024/08/14 16:34:24.000","name":"Ayote de Caballo","cmt":null},"geometry":{"type":"Point","coordinates":[-85.89897,11.34466]}}]}
