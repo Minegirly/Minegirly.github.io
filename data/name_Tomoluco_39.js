2bcc746f44b1bab377cc34a4a7a592d6cd19b757
Yunaax <yuna.morel@orange.fr>
2024-08-23 03:14:54 +0200

    Add files via upload

---
+++ data/name_Tomoluco_39.js
@@ -0,0 +1,1 @@
+var json_name_Tomoluco_39 = {"type":"FeatureCollection","name":"name_Tomoluco_39","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","time":"2024/08/06 10:56:07.000","name":"Tomoluco","cmt":"1"},"geometry":{"type":"Point","coordinates":[-85.918985,11.314969]}}]}
