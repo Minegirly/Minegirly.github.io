b1fa33ad6dd6dc8eba7d0cb4c4b30cda2261d38e
Yunaax <yuna.morel@orange.fr>
2024-08-23 17:41:57 +0200

    Delete data directory

--- data/name_Mapache_36.js
+++
@@ -1,1 +0,0 @@
-var json_name_Mapache_36 = {"type":"FeatureCollection","name":"name_Mapache_36","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","time":"2024/08/08 18:22:17.000","name":"Mapache","cmt":"2"},"geometry":{"type":"Point","coordinates":[-85.909521,11.31686]}}]}
--- data/name_malinche_33.js
+++
@@ -1,1 +0,0 @@
-var json_name_malinche_33 = {"type":"FeatureCollection","name":"name_malinche_33","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"name":"Malinche"},"geometry":{"type":"MultiPoint","coordinates":[[-85.904773934698241,11.316442989355583]]}}]}
